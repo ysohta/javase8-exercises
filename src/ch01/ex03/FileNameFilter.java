302bb236220e5bdad2045b6ad6d51287852e1907
ysohta <yukiohta234@gmail.com>
2015-06-14 21:02:34 +0900

    Commit ch01 ex03

---
+++ src/ch01/ex03/FileNameFilter.java
@@ -0,0 +1,34 @@
+package ch01.ex03;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This class consists of utility method to list files with the specified
+ * extension.
+ * 
+ * @author yukiohta
+ *
+ */
+public class FileNameFilter {
+
+	/**
+	 * Returns list of files with the extension in the directory.
+	 * 
+	 * @param dir
+	 *            directory to be searched
+	 * @param extension
+	 *            file extension
+	 * @return list of files
+	 * @throws NullPointerException
+	 *             if dir is null
+	 */
+	public static List<File> extractFilesWith(File dir, String extension) {
+		Objects.requireNonNull(dir, "dir must not be non null");
+
+		// extension: captured variable in enclosing scope
+		return Arrays.asList(dir.listFiles((d, n) -> n.endsWith(extension)));
+	}
+}
---
+++ test/ch01/ex03/FileNameFilterTest.java
@@ -0,0 +1,42 @@
+package ch01.ex03;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class FileNameFilterTest {
+
+	List<File> files = new ArrayList<>();
+
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+
+	@Before
+	public void setup() throws IOException {
+		files.add(folder.newFile("sample.txt"));
+		files.add(folder.newFile("sample2.txt"));
+		files.add(folder.newFile("sample.html"));
+		files.add(folder.newFile("sample.htm"));
+	}
+
+	@Test
+	public void testGetSubDirsUsingLambda() {
+		List<File> actuals = FileNameFilter.extractFilesWith(folder.getRoot(), "htm");
+		assertThat(actuals, hasItems(files.get(3)));
+	}
+
+	@Test
+	public void testExtractFilesWithMultiple() {
+		List<File> actuals = FileNameFilter.extractFilesWith(folder.getRoot(), "txt");
+		assertThat(actuals, hasItems(files.get(0), files.get(1)));
+	}
+}
