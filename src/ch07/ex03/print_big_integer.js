9dfc4b999011e15a9c3696b4851017103e6021f5
ysohta <yukiohta234@gmail.com>
2015-10-14 23:07:55 +0900

    add comments

--- src/ch07/ex03/print_big_integer.js
+++ src/ch07/ex03/print_big_integer.js
@@ -1,9 +1,11 @@
 /*
  * Usage: jrunscript -f print_big_integer.js
  */
+// b is double precision floating point number (64 bit)
 var b = new java.math.BigInteger('1234567890987654321')
 
 // 1234567890987654400
+// the value is too large to describe precisely
 print('b=' + b)
 
 var mod = b.mod(java.math.BigInteger.TEN)
